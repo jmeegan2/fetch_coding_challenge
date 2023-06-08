39645a779690e9c10878efa5cd1dc7b9232c32c1
James Meegan <58824024+jmeegan2@users.noreply.github.com>
2023-06-08 16:56:03 -0400

    Initial Commit

---
+++ Fetch_iOS_coding_challenge/Fetch_iOS_coding_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_iOS_coding_challengeApp.swift
+//  Fetch_iOS_coding_challenge
+//
+//  Created by James Meegan on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_iOS_coding_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
